fd6d66a3161705f3fa987aea6b37d1cf946a4ebf
Avery knight <averjk@gmail.com>
2024-08-20 21:24:49 -0500

    update to name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const context = github.context;
 
-
 // most @actions toolkit packages have async methods
 async function run() {
     try {
