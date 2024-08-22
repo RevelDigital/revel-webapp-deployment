b4069f62d6538dc45014b9576b6d9241f8f1ebff
Avery knight <averjk@gmail.com>
2024-08-22 18:57:46 -0500

    Added code for uploading github action. Zip/Webapp not working on upload

--- src/index.js
+++ src/index.js
@@ -23,17 +23,26 @@ async function run() {
 
         archive.pipe(output);
         archive.directory('dist/ng-test-client-sdk', false);
-        archive.finalize();
+        await archive.finalize();
         const form = new FormData();
-        form.append('file', fs.createReadStream('target.webapp'));
-        form.append('name', 'target.webapp')
+
+        form.append('file', fs.createReadStream('test.jpg'));
+        form.append('name', 'test.jpg')
         form.append('group_id', 'F463rfkTL46J2BDsk46GmA')
+        form.append('tags', '')
+        form.append('advertiser_id', '')
+        // form.append('is_shared', false)
+        form.append('start_date', '2024-08-21T22:03:39.493Z')
+        form.append('end_date', '2024-08-21T22:03:39.493Z')
 
         const request_config = {
             headers: {
-                ...form.getHeaders()
+                ...form.getHeaders(),
+                "X-Reveldigital-Apikey": "rDJquU_3VaJ9GCu0Dj9K"
             }
         };
+
+        console.log(request_config.headers)
 
         return axios.post(`https://api.reveldigital.com/media?api_key=${"rDJquU_3VaJ9GCu0Dj9K8w"}`, form, request_config)
     } catch (error) {
@@ -42,7 +51,8 @@ async function run() {
 }
 
 run().then(val=>{
-    console.log(val)
+    console.log(val.headers)
+    setInterval(()=>{},50000)
 }).catch(err=>{
     console.log(err)
 });
