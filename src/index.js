554523eaaa32ffd620aacd470f06b658ac815f38
Avery knight <averjk@gmail.com>
2024-08-23 11:58:19 -0500

    finished github action

--- src/index.js
+++ src/index.js
@@ -5,48 +5,90 @@ const archiver = require('archiver');
 const context = github.context;
 const FormData = require('form-data');
 const axios = require('axios');
+process.env['version'] = 'my-value';
+
+// core.getInput = (val)=>{
+//     const test = {
+//         version: '',
+//         name: 'test',
+//         "group-name": "",
+//         "environment":"Production",
+//         "api-key":"",
+//         tags: "test tags",
+//         "distribution-location": "dist/ng-test-client-sdk"
+//     }
+//     return test[val]
+// }
 // most @actions toolkit packages have async methods
 async function run() {
     try {
-        core.getInput('api-key')
-        const output = fs.createWriteStream('target.webapp');
-        const archive = archiver('zip');
+        axios.get(`https://api.reveldigital.com/media/groups?api_key=${core.getInput('api-key')}&tree=false`).then((groups)=>{
+            const output = fs.createWriteStream(core.getInput('name')+'.webapp');
+            const archive = archiver('zip');
 
-        output.on('close', function () {
-            console.log(archive.pointer() + ' total bytes');
-            console.log('archiver has been finalized and the output file descriptor has closed.');
-            const form = new FormData();
-
-            form.append('file', fs.createReadStream('target.webapp'));
-            form.append('name', 'target.webapp')
-            form.append('group_id', 'F463rfkTL46J2BDsk46GmA')
-            form.append('tags', '')
-            form.append('is_shared', 'false')
-
-            const request_config = {
-                headers: {
-                    ...form.getHeaders(),
-                    "X-Reveldigital-Apikey": "rDJquU_3VaJ9GCu0Dj9K"
+            output.on('close',  ()=> {
+                let version;
+                console.log(archive.pointer() + ' total bytes');
+                console.log('archiver has been finalized and the output file descriptor has closed.');
+                const form = new FormData();
+                if(core.getInput('version')){
+                    version = `\nversion=${core.getInput('version')}`
                 }
-            };
+                else if(fs.existsSync('package.json')){
+                    version = ''
+                    try{
+                        const tmp = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+                        version = `\nversion=${tmp.version}`;
+                    }catch (e) {
+                        console.log(e)
+                    }
+                }else{
+                    version = ''
+                }
+                console.log(version)
+                form.append('file', fs.createReadStream(core.getInput('name')+'.webapp'));
+                form.append('name', core.getInput('name')+'.webapp')
+                if(core.getInput('group-name')){
+                    let tmp = groups.data[0].id
+                    for(const val of groups.data){
+                        if(val.name.search(core.getInput('group-name'))>-1){
+                            console.log(val.name, 'match')
+                            tmp = val.id
+                            break;
+                        }
+                    }
+                    form.append('group_id', tmp)
+                }
+                else{
+                    form.append('group_id', groups.data[0].id)
+                }
+                form.append('tags', core.getInput('tags')+`${version}\nenv=${core.getInput('environment')}`)
+                form.append('is_shared', 'false')
 
-            console.log(request_config.headers)
+                const request_config = {
+                    headers: {
+                        ...form.getHeaders(),
+                        "X-Reveldigital-Apikey": core.getInput('api-key')
+                    }
+                };
 
-            axios.post(`https://api.reveldigital.com/media?api_key=${"rDJquU_3VaJ9GCu0Dj9K8w"}`, form, request_config).then(val=>{
-                console.log(val.status)
-            }).catch(err=>{
-                console.log(err)
+                axios.post(`https://api.reveldigital.com/media?api_key=${core.getInput('api-key')}`, form, request_config).then(val=>{
+                    console.log(val.status)
+                }).catch(err=>{
+                    console.log('failed to upload')
+                });
+
             });
 
-        });
+            archive.on('error', function(err){
+                throw err;
+            });
 
-        archive.on('error', function(err){
-            throw err;
-        });
+            archive.pipe(output);
+            archive.directory(core.getInput('distribution-location'), false);
+            archive.finalize();
+        })
 
-        archive.pipe(output);
-        archive.directory('dist/ng-test-client-sdk', false);
-        await archive.finalize();
 
     } catch (error) {
         core.setFailed(error.message);
