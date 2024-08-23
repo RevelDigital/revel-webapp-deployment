91ca059cf93f49ac6f6d129358f5d705771e57ac
Avery knight <averjk@gmail.com>
2024-08-22 19:44:49 -0500

    update for webapp

--- src/index.js
+++ src/index.js
@@ -15,6 +15,29 @@ async function run() {
         output.on('close', function () {
             console.log(archive.pointer() + ' total bytes');
             console.log('archiver has been finalized and the output file descriptor has closed.');
+            const form = new FormData();
+
+            form.append('file', fs.createReadStream('target.webapp'));
+            form.append('name', 'target.webapp')
+            form.append('group_id', 'F463rfkTL46J2BDsk46GmA')
+            form.append('tags', '')
+            form.append('is_shared', 'false')
+
+            const request_config = {
+                headers: {
+                    ...form.getHeaders(),
+                    "X-Reveldigital-Apikey": "rDJquU_3VaJ9GCu0Dj9K"
+                }
+            };
+
+            console.log(request_config.headers)
+
+            axios.post(`https://api.reveldigital.com/media?api_key=${"rDJquU_3VaJ9GCu0Dj9K8w"}`, form, request_config).then(val=>{
+                console.log(val.status)
+            }).catch(err=>{
+                console.log(err)
+            });
+
         });
 
         archive.on('error', function(err){
@@ -24,35 +47,10 @@ async function run() {
         archive.pipe(output);
         archive.directory('dist/ng-test-client-sdk', false);
         await archive.finalize();
-        const form = new FormData();
 
-        form.append('file', fs.createReadStream('test.jpg'));
-        form.append('name', 'test.jpg')
-        form.append('group_id', 'F463rfkTL46J2BDsk46GmA')
-        form.append('tags', '')
-        form.append('advertiser_id', '')
-        // form.append('is_shared', false)
-        form.append('start_date', '2024-08-21T22:03:39.493Z')
-        form.append('end_date', '2024-08-21T22:03:39.493Z')
-
-        const request_config = {
-            headers: {
-                ...form.getHeaders(),
-                "X-Reveldigital-Apikey": "rDJquU_3VaJ9GCu0Dj9K"
-            }
-        };
-
-        console.log(request_config.headers)
-
-        return axios.post(`https://api.reveldigital.com/media?api_key=${"rDJquU_3VaJ9GCu0Dj9K8w"}`, form, request_config)
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
-run().then(val=>{
-    console.log(val.headers)
-    setInterval(()=>{},50000)
-}).catch(err=>{
-    console.log(err)
-});
+run()
